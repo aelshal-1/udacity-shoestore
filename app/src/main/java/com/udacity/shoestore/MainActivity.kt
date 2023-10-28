3549e9f49b6dd1544e34c113e476135d6efacadd
Ahmed Elshal <59453118+aelshal-1@users.noreply.github.com>
2023-10-28 23:14:05 +0200

    setting actionBar

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -3,6 +3,9 @@ package com.udacity.shoestore
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
@@ -11,7 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.loginFragment,R.id.welcomeFragment))
+        NavigationUI.setupActionBarWithNavController(this, navController,appBarConfiguration)
+
         Timber.i("onCreate")
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController= this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 
     override fun onStart() {
