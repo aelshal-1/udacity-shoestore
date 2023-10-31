2fc90d0e41643437a31073163287081e3a93d386
Ahmed Elshal <59453118+aelshal-1@users.noreply.github.com>
2023-10-31 22:41:57 +0200

    modify Shoe Details fragment

--- app/src/main/java/com/udacity/shoestore/shoeDetails/ShoeDetailsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/shoeDetails/ShoeDetailsFragment.kt
@@ -7,55 +7,60 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailsBinding
 
 
 class ShoeDetailsFragment : Fragment() {
-    private lateinit var binding :FragmentShoeDetailsBinding
+    private lateinit var binding: FragmentShoeDetailsBinding
     private lateinit var viewModel: ShowDetailsViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
+        arguments?.let {}
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_shoe_details,container,false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_details, container, false)
 
         viewModel = ViewModelProvider(this)[ShowDetailsViewModel::class.java]
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
 
-        binding.saveButton.setOnClickListener { view ->
-
-            viewModel.validate(binding.shoeNameEdit.text.toString(),
-                binding.companyEdit.text.toString(),
-                binding.detailsEdit.text.toString())
-
-            if (viewModel.isValidateSuccess.value == true) {
-                view.findNavController().navigate(ShoeDetailsFragmentDirections.actionShoeDetailsFragmentToShoeListFragment().apply {
-                    shoeName = binding.shoeNameEdit.text.toString()
-                    companyName = binding.companyEdit.text.toString()
-                    shoeSize = binding.shoeSizeEdit.text.toString().toInt()
-                    shoeDescription =  binding.detailsEdit.text.toString()
-                })
-            }else{
-                Toast.makeText(this.context,viewModel.errorMessage.value, Toast.LENGTH_LONG).show()
-            }
-
-
-        }
-
+        //cancel
         binding.cancelButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(ShoeDetailsFragmentDirections.actionShoeDetailsFragmentToShoeListFragment())
         )
 
+
+        //Show Error
+        viewModel.errorMessage.observe(viewLifecycleOwner, Observer { errorMessage ->
+            if(errorMessage.isNotEmpty())
+                Toast.makeText(this.context, errorMessage, Toast.LENGTH_LONG).show()
+        })
+
+
+        // Save
+        viewModel.saveEventComplete.observe(viewLifecycleOwner, Observer { isSave ->
+            if (isSave) {
+                findNavController().navigate(ShoeDetailsFragmentDirections.actionShoeDetailsFragmentToShoeListFragment()
+                    .apply {
+                        shoeName = binding.shoeNameEdit.text.toString()
+                        companyName = binding.companyEdit.text.toString()
+                        shoeSize = binding.shoeSizeEdit.text.toString().toInt()
+                        shoeDescription = binding.detailsEdit.text.toString()
+                    })
+                viewModel.onSaveEventComplete()
+            }
+        })
 
         return binding.root
     }
--- app/src/main/java/com/udacity/shoestore/shoeDetails/ShowDetailsViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/shoeDetails/ShowDetailsViewModel.kt
@@ -4,28 +4,45 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ShowDetailsViewModel :ViewModel() {
+class ShowDetailsViewModel : ViewModel() {
 
-    private val _isValidateSuccess = MutableLiveData<Boolean>()
-    val isValidateSuccess : LiveData<Boolean>
-        get() = _isValidateSuccess
+    val shoeName = MutableLiveData<String>()
+    val companyName = MutableLiveData<String>()
+    val shoeSize = MutableLiveData<String>()
+    val shoeDetails = MutableLiveData<String>()
 
     private val _errorMessage = MutableLiveData<String>()
-    val errorMessage : LiveData<String>
+    val errorMessage: LiveData<String>
         get() = _errorMessage
 
+    private val _saveEventComplete = MutableLiveData<Boolean>()
+    val saveEventComplete: LiveData<Boolean>
+        get() = _saveEventComplete
+
     init {
-        _isValidateSuccess.value = false
+        shoeName.value = ""
+        shoeSize.value = "1"
+        companyName.value = ""
+        shoeDetails.value = ""
+        _errorMessage.value = ""
+        _saveEventComplete.value = false
     }
-    fun validate(name:String, company:String, details:String){
-        if(name.isEmpty()){
-            _errorMessage.value ="Shoe Name is Empty"
-        }else if(company.isEmpty()){
-            _errorMessage.value ="Company Name is Empty"
-        }else if(details.isEmpty()){
-            _errorMessage.value ="details is Empty"
-        }else {
-            _isValidateSuccess.value = true
+
+    fun save() {
+        if (shoeName.value!!.isEmpty()) {
+            _errorMessage.value = "Shoe Name is Empty"
+        } else if (companyName.value!!.isEmpty()) {
+            _errorMessage.value = "company Name is Empty"
+        } else if (shoeSize.value!!.isEmpty()) {
+            _errorMessage.value = "Shoe Size is Empty"
+        } else if (shoeDetails.value!!.isEmpty()) {
+            _errorMessage.value = "Shoe Details is Empty"
+        } else {
+            _saveEventComplete.value = true
         }
+    }
+
+    fun onSaveEventComplete() {
+        _saveEventComplete.value = false
     }
 }
